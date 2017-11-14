9e5aaf2c3e000d23efda4e20e4a5935e327d19be
Jackie Baxter <jackeye@gmail.com>
2017-11-14 00:36:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
   return "I can't hear you!"
 }
 
 function sayHiToGrandma(string){
-  if (string.toUpperCase() === string)
+  if (string.toUpperCase() === string) {
   return "YES INDEED!"
 }
