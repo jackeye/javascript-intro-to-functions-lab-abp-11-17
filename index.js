4e0a4d1333d0234d7951ec67fadcf1a55541e86c
Jackie Baxter <jackeye@gmail.com>
2017-11-14 00:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ function sayHiToGrandma(string){
   return "I can't hear you!"
 }
 
-  if (string.toUpperCase() === string)
-  return "YES INDEED!"
+    if (string.toUpperCase() === string)
+    return "YES INDEED!"
+}
+
+    if (string === "I love you Grandma.") {
+    return "I love you too!"
+}
+    return "Are you eating enough?"
+
 }
